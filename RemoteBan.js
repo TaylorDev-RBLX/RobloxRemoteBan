e73ac50285ee44ac54650fea78be7c633a3831d6
TaylorDev_RBLX <69086628+TaylorDev-RBLX@users.noreply.github.com>
2025-08-17 14:24:00 +0100

    Update RemoteBan.js

--- RemoteBan.js
+++ RemoteBan.js
@@ -66,4 +66,6 @@ app.get("/ban/:universeId/:placeId/:userId", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Ban server running on port 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Ban server running on port ${port}`));
+
